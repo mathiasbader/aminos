c80ef92413cbbbfba6bbde1e72b6db63e2397ad4
Mathias Bader <mail@mathiasbader.de>
2021-09-11 12:10:01 +0200

    Add image to name module

---
+++ src/Constant/Common.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace App\Constant;
+
+class Common
+{
+    const AMINOS_COUNT = 20;
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,11 @@
 namespace App\Controller;
 
 
+use App\Constant\Common;
 use App\Entity\Aminoacid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -16,7 +18,7 @@ class DefaultController extends AbstractController
     public function indexAction()
     {
         return [
-            'pageTitle' => 'The 20 proteinogenic amino acids',
+            'pageTitle' => 'The ' . Common::AMINOS_COUNT . ' proteinogenic amino acids',
         ];
     }
 
@@ -25,9 +27,39 @@ class DefaultController extends AbstractController
     {
         $aminos = $this->getDoctrine()->getRepository(Aminoacid::class)->findAll();
         return [
-            'pageTitle' => 'The 20 proteinogenic amino acids',
+            'pageTitle' => 'The ' . Common::AMINOS_COUNT . ' proteinogenic amino acids',
             'aminos'    => $aminos,
             'bigger'     => $param === 'b',
+        ];
+    }
+
+    /** @Route("/i2n", name="imgToName") @Template */
+    public function imgToNameAction(Request $request)
+    {
+        $answerText    = $request->get('answer');
+        $answerAminoId = $request->get('amino');
+
+        $answerCorrect = null;
+        $answerAmino   = null;
+        if (!empty($answerText) && $answerAminoId !== null && is_numeric($answerAminoId)) {
+            /* @var $answerAmino Aminoacid */
+            $answerAmino = $this->getDoctrine()->getRepository(Aminoacid::class)->find($answerAminoId);
+
+            $answerLower = mb_strtolower($answerText);
+            $answerCorrect =
+                $answerLower === mb_strtolower($answerAmino->getCode1 ()) ||
+                $answerLower === mb_strtolower($answerAmino->getCode3 ()) ||
+                $answerLower === mb_strtolower($answerAmino->getNameEn()) ||
+                $answerLower === mb_strtolower($answerAmino->getNameDe());
+        }
+
+        $amino = $this->getDoctrine()->getRepository(Aminoacid::class)->find(rand(1, Common::AMINOS_COUNT));
+        return [
+            'pageTitle'     => 'Image to name - The ' . Common::AMINOS_COUNT . ' proteinogenic amino acids',
+            'amino'         => $amino,
+            'answerText'    => $answerText,
+            'answerAmino'   => $answerAmino,
+            'answerCorrect' => $answerCorrect,
         ];
     }
 
