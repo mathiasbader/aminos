3c7510b0da209336cdf9285ef00367db5d5d2497
Mathias Bader <mail@mathiasbader.de>
2021-10-06 20:42:24 +0200

    Remove unused file login.html.twig; Add translations

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,9 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
+    /** @Route("/login", name="app_login") */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
@@ -20,9 +18,7 @@ class SecurityController extends AbstractController
         return $this->render('default/profile.html.twig', ['loginError' => $loginError]);
     }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
+    /** @Route("/logout", name="app_logout") */
     public function logout()
     {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
